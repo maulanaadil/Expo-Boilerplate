c8c545e99051311799f45f636f7b513785bfb856
Maulana Adil <maulanaadil.latif@gmail.com>
2023-06-16 14:49:55 +0700

    feat: init project

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Text, SafeAreaView } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-white items-center justify-center">
-      <Text className="text-black">Open up App.tsx to start working on your app!</Text>
+      <Text className="text-black">Hello World</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
---
+++ README.md
@@ -0,0 +1,11 @@
+# Expo Typescript Tailwind Boilerplate
+This boilerplate features Expo (w/ Typescript) + Tailwind (Nativewind) + Linting already setup with Prettier and Eslint + Husky (pre-commits)
+
+### Commands
+```
+yarn install
+yarn start
+yarn ios/android
+yarn lint
+yarn setup-husky (link the pre-commit hooks to git)
+```
